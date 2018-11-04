00eee60d65aa9094c2a6eb51a2bdc04377cd512b
VladTarasevich <44198549+VladTarasevich@users.noreply.github.com>
2018-11-04 14:54:25 +0300

    Add files via upload

---
+++ Auto.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace OOP
+{
+    class Auto
+    {
+        private string mark;
+        private string model;
+        private int amount;
+        private int price;
+        public Auto() 
+        {
+            Console.WriteLine("New car");
+            SetMark();
+            SetModel();
+            SetAmount();
+            SetCost();
+            Console.WriteLine();
+        }
+        private void SetMark() 
+        {
+            char[] markChar = new char[80]; 
+            bool empty = false; 
+            do
+            {
+                try
+                {
+                    Console.Write("Mark: ");
+                    mark = Console.ReadLine();
+                    markChar = mark.ToCharArray();
+
+                    for (int i = 0; i < markChar.Length; i++)
+                    {
+                        if (markChar[0] == ' ') 
+                        {
+                            break;
+                        }
+                        if (Char.IsLetterOrDigit(markChar[i]) == true) 
+                        {
+                            empty = true;
+                            break;
+                        }
+                    }
+                    if (empty == true)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect input, check your data");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect input, check your data");
+                }
+            }
+            while (true);
+        }
+        private void SetModel() 
+        {
+            char[] modelChar = new char[80]; 
+            bool Empty = false; 
+            do
+            {
+                try
+                {
+                    Console.Write("Model: ");
+                    model = Console.ReadLine();
+                    modelChar = model.ToCharArray();
+                    for (int i = 0; i < modelChar.Length; i++)
+                    {
+                        if (modelChar[0] == ' ') 
+                        {
+                            break;
+                        }
+                        if (Char.IsLetterOrDigit(modelChar[i]) == true) 
+                        {
+                            Empty = true;
+                            break;
+                        }
+                    }
+                    if (Empty == true)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect input, check your data");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect input, check your data");
+                }
+            }
+            while (true);
+        }
+        private void SetAmount()
+        {
+            do
+            {
+                try
+                {
+                    Console.Write("Amount: "); 
+                    amount = Convert.ToInt32(Console.ReadLine());
+                    if (amount > 0)
+                    {
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Incorrect input, check your data");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect input, check your data");
+                }
+            }
+            while (true);
+        }
+        private void SetCost() 
+        {
+            do
+            {
+                try
+                {
+                    Console.Write("Price: ");
+                    price = Convert.ToInt32(Console.ReadLine());
+                    if (price > 0) 
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect input, check your data");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect input, check your data");
+                }
+            }
+            while (true);
+        }
+        public int getAmount()
+        {
+            return amount;
+        }
+        public int getCost()
+        {
+            return price;
+        }
+        public string getMark() 
+        {
+            return mark;
+        }
+        public string getModel() 
+        {
+            return model;
+        }
+        public void RepeatAmountSet(int plusAmount) 
+        {
+            amount += plusAmount;
+        }
+    }
+}
---
+++ AutoData.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP
+{
+    class AutoData
+    {
+        List<Auto> List = new List<Auto>();
+        public void AddAuto() 
+        {
+            List.Add(new Auto());
+            Repeat();
+        }
+        
+        public int CountTypes() 
+        {
+            int Marks = 0;
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (DoubleCheck(i) == true) 
+                {
+                    Marks++;
+                }
+            }
+            return Marks;
+        }
+        private bool DoubleCheck(int iteration) 
+        {
+            bool check = true;
+            if (iteration != 0)
+            {
+                for (int i = 0; i < iteration; i++)
+                {
+                    if (List[iteration].getMark() == List[i].getMark())
+                    {
+                        check = false;
+                    }
+                }
+            }
+            return check;
+        }
+        private void Repeat()
+        {
+            int j = List.Count - 1;
+            for (int i = 0; i < j; i++)
+            {
+                if ((List[i].getMark() == List[j].getMark()) && (List[i].getModel() == List[j].getModel()) &&
+                    (List[i].getCost() == List[j].getCost()))
+                {
+                    List[i].RepeatAmountSet(List[j].getAmount());
+                    List.RemoveAt(j);
+                    break;
+                }
+            }
+        }
+        public int Count() 
+        {
+            int Amount = 0;
+            for (int i = 0; i < List.Count; i++)
+            {
+                Amount += List[i].getAmount();
+            }
+            return Amount;
+        }
+        public int Cost() 
+        {
+            int Cost = 0;
+            for (int i = 0; i < List.Count; i++)
+            {
+                Cost += List[i].getCost();
+            }
+            return Cost;
+        }
+        public double AveragePrice() 
+        {
+            
+            double tempCostAll = Convert.ToDouble(Cost());
+            double tempCountAll = Convert.ToDouble(Count());
+            double averagePrice = tempCostAll / tempCountAll;
+            return averagePrice;
+        }
+        public double AveragePrice(string type) 
+        {
+            
+            double tempCost = 0;
+            double tempCount = 0;
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].getMark() == type)
+                {
+                    tempCost += Convert.ToDouble(List[i].getCost());
+                    tempCount += Convert.ToDouble(List[i].getAmount());
+                }
+            }
+            double typeAveragePrice = tempCost / tempCount;
+            return typeAveragePrice;
+        }
+    }
+}
+    
